de1573f72e9993b42e16e4f14a671364be43b623
Karima Rafes <karima.rafes@gmail.com>
2018-04-22 22:48:44 +0200

    fix

--- FindWikidataIdWithFewData/script.sh
+++ FindWikidataIdWithFewData/script.sh
@@ -3,4 +3,4 @@
 spark-submit --class "App" \
              --packages org.apache.pdfbox:pdfbox:2.0.9,org.scalaj:scalaj-http_2.11:2.3.0 \
              --master local \
-             target/scala-2.11/halwikidata_2.11-0.1.jar pdf/exemple.pdf
+             target/scala-2.11/findidwikidatawithfewdata_2.11-0.1.jar pdf/exemple.pdf
